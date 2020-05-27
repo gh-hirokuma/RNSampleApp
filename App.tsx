14104632e11840a092982dc0f06bfcf1bea3fb64
hiroshige.negishi <hirosige1@gmail.com>
2020-05-27 15:02:11 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, View, Text, StatusBar } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View style={{ padding: 30 }}>
+          <Text style={{ fontSize: 32 }}>サンプルアプリ</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
